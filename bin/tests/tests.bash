798f0f75e1b97d67551ade1bedf5fbcbc9482a33
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-11-28 16:23:13 +0900

    tests: Rename data filename

--- bin/tests/tests.bash
+++ bin/tests/tests.bash
@@ -14,15 +14,15 @@ for i in awk sed pl; do
   ldifunwrap_cmd="ldifunwrap.$i"
   echo "Test: $ldifunwrap_cmd with valid LDIF data ..."
   for a_ldif in */*.ldif; do
-    [[ $a_ldif == */broken-* ]] && continue
+    [[ $a_ldif == */invalid-* ]] && continue
     for b_ldif in */*.ldif; do
-      [[ $b_ldif == */broken-* ]] && continue
+      [[ $b_ldif == */invalid-* ]] && continue
       "$ldifunwrap_cmd" <"$a_ldif" |ldifdiff.py "$b_ldif" /dev/stdin
     done
   done
 
-  echo "Test: $ldifunwrap_cmd with broken LDIF data ..."
-  for a_ldif in data/broken-*.ldif; do
+  echo "Test: $ldifunwrap_cmd with invalid LDIF data ..."
+  for a_ldif in data/invalid-*.ldif; do
     # shellcheck disable=SC2094
     # Make sure not to read and write the same file in the same pipeline.
     "$ldifunwrap_cmd" <"$a_ldif" |diff -u "$a_ldif" -
