7f802ac3256c3da8b4fc6aff8fb4ffdf48745a38
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-03-07 15:57:14 +0900

    tests: Test with multiple entries in LDIF data

--- bin/tests/tests.bash
+++ bin/tests/tests.bash
@@ -12,18 +12,37 @@ ldifunwrap.pl() {
 
 for i in awk sed pl; do
   ldifunwrap_cmd="ldifunwrap.$i"
-  echo "Test: $ldifunwrap_cmd with valid LDIF data ..."
   for a_ldif in */one-entry-*.ldif; do
     for b_ldif in */one-entry-*.ldif; do
-      "$ldifunwrap_cmd" <"$a_ldif" |ldifdiff.py "$b_ldif" /dev/stdin
-      "$ldifunwrap_cmd" <"$a_ldif" |ldifdiff.pl "$b_ldif" /dev/stdin
+      for ldifdiff_cmd in ldifdiff.py ldifdiff.pl; do
+        echo "Test: $ldifunwrap_cmd: $ldifdiff_cmd: Valid LDIF data: ${a_ldif##*/}, ${b_ldif##*/}"
+        "$ldifunwrap_cmd" \
+          <"$a_ldif" \
+        |"$ldifdiff_cmd" \
+          "$b_ldif" \
+          /dev/stdin \
+        ;
+        "$ldifunwrap_cmd" \
+          < <(
+            cat "$a_ldif"
+            echo
+            sed 's/^dn:[^,]*/&2/' "$b_ldif"
+          ) \
+        |"$ldifdiff_cmd" \
+          <(
+            sed 's/^dn:[^,]*/&2/' "$b_ldif"
+            echo
+            cat "$a_ldif"
+          ) \
+          /dev/stdin \
+        ;
+      done
     done
   done
 
-  echo "Test: $ldifunwrap_cmd with invalid LDIF data ..."
   for a_ldif in data/invalid-*.ldif; do
-    # shellcheck disable=SC2094
-    # Make sure not to read and write the same file in the same pipeline.
+    echo "Test: $ldifunwrap_cmd: Invalid LDIF data: ${a_ldif##*/}"
+    # shellcheck disable=SC2094 # Make sure not to read and write the same file in the same pipeline
     "$ldifunwrap_cmd" <"$a_ldif" |diff -u "$a_ldif" -
   done
 done
