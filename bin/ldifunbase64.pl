9729b324ab520353321b67e368b95271db087434
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-03-13 21:30:54 +0900

    feat: ldifunbase64: Add --comment and --help options

--- bin/ldifunbase64.pl
+++ bin/ldifunbase64.pl
@@ -11,17 +11,32 @@ use warnings;
 use Encode;
 use MIME::Base64;
 
+use constant TRUE => !! 1;
+use constant FALSE => !! '';
+
 sub unbase64
 {
-  my ($name, $value_b64) = @_;
+  my ($name, $value_b64, $as_comment) = @_;
 
   my $value = MIME::Base64::decode($value_b64);
   my $value_utf8 = eval {
     Encode::decode_utf8(my $value_tmp=$value, Encode::FB_CROAK);
   };
 
-  unless (!$@ && $value_utf8 =~ /\A[\p{IsPrint}\t\n\r\e]*\z/) {
-    return "${name}::$value_b64";
+  if ($@) {
+    my $line = "${name}::$value_b64";
+    if ($as_comment) {
+      return "## ${name}:: BINARY\n$line";
+    }
+    return $line
+  }
+
+  if ($value_utf8 !~ /\A[\p{IsPrint}\t\n\r\e]*\z/) {
+    my $line = "${name}::$value_b64";
+    if ($as_comment) {
+      return "## ${name}:: BINARY\n$line";
+    }
+    return $line
   }
 
   $value =~ s/\\/\\\\/g;
@@ -30,11 +45,28 @@ sub unbase64
   $value =~ s/\r/\\r/g;
   $value =~ s/\e/\\e/g;
 
-  return "$name: $value\n";
+  my $line = "$name: $value\n";
+  if ($as_comment) {
+    return "## $line${name}::$value_b64";
+  }
+  return $line
+}
+
+my $as_comment = FALSE;
+
+if ($ARGV[0]) {
+  if ($ARGV[0] eq '-h' || $ARGV[0] eq '--help') {
+    print("Usage: $0 [--comment] < data.ldif\n");
+    exit(0);
+  }
+  if ($ARGV[0] eq '--comment') {
+    $as_comment = TRUE;
+    shift(@ARGV);
+  }
 }
 
 $/ = "\n\n";
 while (<>) {
-  s/^(\w[\w\-]*(?:;\w[\w\-]*)*)::((?:[ \t]*[^\n]*\n)(?:[ \t]+[^\n]*\n)*)/unbase64($1, $2)/mge;
+  s/^(\w[\w\-]*(?:;\w[\w\-]*)*)::((?:[ \t]*[^\n]*\n)(?:[ \t]+[^\n]*\n)*)/unbase64($1, $2, $as_comment)/mge;
   print;
 }
